279bb682a786adbb19b88a08623ec147dfe2cdab
tunze <tunde4excel@gmail.com>
2021-01-22 12:34:53 +0100

    Spring bean scopes and lifecycle

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -21,5 +21,14 @@ public class TrackCoach implements Coach {
         return "just do it "+ fortuneService.getFortune();
     }
 
+    //add an init method for starting up a bean (in here u can add any custom code before it runs the main program)
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside method");
+    }
 
+    //add a destroy method
+    public void doCleanUpStuff()
+    {
+        System.out.println("TrackCoach: inside method clean up stuff");
+    }
 }
